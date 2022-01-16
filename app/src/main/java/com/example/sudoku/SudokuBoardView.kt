76c5919487a2e2ad1383a1fe7b3f2a701758a41e
timothylidede <lidedetimothy@gmail.com>
2022-01-17 00:38:27 +0300

    thickpaintline func

--- app/src/main/java/com/example/sudoku/SudokuBoardView.kt
+++ app/src/main/java/com/example/sudoku/SudokuBoardView.kt
@@ -2,12 +2,20 @@ package com.example.sudoku
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
+    private val thickLinePaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.BLACK
+        strokeWidth = 4F
+    }
+
     override fun onDraw(canvas: Canvas){
-        
+
     }
 }
