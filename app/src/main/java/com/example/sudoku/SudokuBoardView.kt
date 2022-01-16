2c38b713b260bec9f943f868265bd622bd1b5648
timothylidede <lidedetimothy@gmail.com>
2022-01-17 00:34:35 +0300

    func onDraw

--- app/src/main/java/com/example/sudoku/SudokuBoardView.kt
+++ app/src/main/java/com/example/sudoku/SudokuBoardView.kt
@@ -1,4 +1,13 @@
 package com.example.sudoku
 
-class SudokuBoardView {
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+
+class SudokuBoardView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+
+    override fun onDraw(canvas: Canvas){
+        
+    }
 }
