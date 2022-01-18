fd9adeff17a12ae4c07fa4736e193e91eee231a5
timothylidede <lidedetimothy@gmail.com>
2022-01-19 02:22:43 +0300

    initializing cellSizePixels

--- app/src/main/java/com/example/sudoku/SudokuBoardView.kt
+++ app/src/main/java/com/example/sudoku/SudokuBoardView.kt
@@ -13,7 +13,7 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) :
     private var sqrtSize = 3
     private var size = 9
 
-    private var cellSizePixels = 4F
+    private var cellSizePixels = 0F
 
     private val thickLinePaint = Paint().apply {
         style = Paint.Style.STROKE
@@ -28,7 +28,11 @@ class SudokuBoardView(context: Context, attributeSet: AttributeSet) :
     }
 
     override fun onDraw(canvas: Canvas){
+        cellSizePixels = (width/size).toFloat()
+
         canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(),
             thickLinePaint)
+
+        drwLine(canvas)
     }
 }
