bc1098455c2f2108ddc49543be09cc52aca989c2
timothylidede <lidedetimothy@gmail.com>
2022-01-23 01:26:56 +0300

    starting playsudokuactivity

--- app/src/main/java/com/example/sudoku/StartActivity.kt
+++ app/src/main/java/com/example/sudoku/StartActivity.kt
@@ -1,11 +1,19 @@
 package com.example.sudoku
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val startCard = findViewById<CardView>(R.id.start_card)
+        startCard.setOnClickListener {
+            val intent = Intent(this, PlaySudokuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
