662e289016c06ad713aacce6402cc44fed065668
timothylidede <lidedetimothy@gmail.com>
2022-01-24 23:29:59 +0300

    initializing live data val

--- app/src/main/java/com/example/sudoku/game/SudokuGame.kt
+++ app/src/main/java/com/example/sudoku/game/SudokuGame.kt
@@ -6,4 +6,11 @@ class SudokuGame {
 
     var selectedCellLiveData = MutableLiveData<Pair<Int, Int>>()
 
+    private var selectedRow = -1
+    private var selectedCol = -1
+
+    init {
+        selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
+    }
+
 }
