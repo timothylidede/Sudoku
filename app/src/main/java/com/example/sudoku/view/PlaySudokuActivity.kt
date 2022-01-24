6df5d652e8c056a770e12e9ee7401b06ea6d406f
timothylidede <lidedetimothy@gmail.com>
2022-01-25 02:23:11 +0300

    updatedSelectedCellUI fun

--- app/src/main/java/com/example/sudoku/view/PlaySudokuActivity.kt
+++ app/src/main/java/com/example/sudoku/view/PlaySudokuActivity.kt
@@ -2,9 +2,12 @@ package com.example.sudoku.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.sudoku.R
 import com.example.sudoku.viewmodel.PlaySudokuViewModel
+import kotlinx.android.synthetic.main.activity_play_sudoku.*
+
 
 class PlaySudokuActivity : AppCompatActivity() {
 
@@ -16,5 +19,12 @@ class PlaySudokuActivity : AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this)
             .get(PlaySudokuViewModel::class.java)
+        viewModel.sudokuGame.selectedCellLiveData.observe(this, Observer { 
+            updateSelectedCellUI(it)
+        })
+    }
+
+    private fun updateSelectedCellUI(cell: Pair<Int, Int>?) = cell?.let{
+        sudokuBoardView.updateSelectedCellUI(cell.first, cell.second)
     }
 }
--- app/src/main/java/com/example/sudoku/viewmodel/PlaySudokuViewModel.kt
+++ app/src/main/java/com/example/sudoku/viewmodel/PlaySudokuViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.sudoku.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.sudoku.game.SudokuGame
 
 class PlaySudokuViewModel: ViewModel() {
     val sudokuGame = SudokuGame()
