abafe5686a3f59dd06fd636b78f7bf6c93462f8e
timothylidede <lidedetimothy@gmail.com>
2022-01-27 23:36:16 +0300

    val cells

--- app/src/main/java/com/example/sudoku/game/SudokuGame.kt
+++ app/src/main/java/com/example/sudoku/game/SudokuGame.kt
@@ -9,7 +9,11 @@ class SudokuGame {
     private var selectedRow = -1
     private var selectedCol = -1
 
+    private val board: Board
+
     init {
+        val cells = List(9 * 9){i -> Cell(i / 9 , i % 9, i % 9)}
+
         selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
     }
 
