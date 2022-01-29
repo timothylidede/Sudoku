2a4697c4a664323326f56877f2e9b6eed330cc81
timothylidede <lidedetimothy@gmail.com>
2022-01-29 23:50:05 +0300

    adding edit icon

--- app/src/main/java/com/example/sudoku/game/Cell.kt
+++ app/src/main/java/com/example/sudoku/game/Cell.kt
@@ -4,7 +4,7 @@ class Cell(
     val row: Int,
     val col: Int,
     var value: Int,
-    var isStartingCell: Boolean = false
+    var isStartingCell: Boolean = false,
     var notes: MutableSet<Int> = mutableSetOf()
 ) {
 }
