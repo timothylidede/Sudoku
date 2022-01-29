f4b988f8e84b35f6bfd31080c2ecff2177a2c0d0
timothylidede <lidedetimothy@gmail.com>
2022-01-29 22:26:37 +0300

    isStartingCell var

--- app/src/main/java/com/example/sudoku/game/Cell.kt
+++ app/src/main/java/com/example/sudoku/game/Cell.kt
@@ -1,4 +1,9 @@
 package com.example.sudoku.game
 
-class Cell(val row: Int, val col: Int, var value: Int) {
+class Cell(
+    val row: Int,
+    val col: Int,
+    var value: Int,
+    var isStartingCell: Boolean = false
+) {
 }
