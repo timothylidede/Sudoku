393e3429b942534b24a29855a6ca255d5183c476
timothylidede <lidedetimothy@gmail.com>
2022-01-31 04:13:46 +0300

    initializing notes live data

--- app/src/main/java/com/example/sudoku/game/SudokuGame.kt
+++ app/src/main/java/com/example/sudoku/game/SudokuGame.kt
@@ -11,17 +11,19 @@ class SudokuGame {
 
     private var selectedRow = -1
     private var selectedCol = -1
+    private var isTakingNotes = false
 
     private val board: Board
 
     init {
         val cells = List(9 * 9){i -> Cell(i / 9 , i % 9, i % 9)}
-        cells[11].isStartingCell = true
-        cells[21].isStartingCell = true
+        cells[0].notes = mutableSetOf(1,2,3,4,5,6,7,8,9)
         board = Board(9, cells)
 
         selectedCellLiveData.postValue(Pair(selectedRow, selectedCol))
         cellsLiveData.postValue(board.cells)
+
+        isTakingNotesLiveData.postValue(isTakingNotes)
     }
 
     fun handleInput(number: Int){
